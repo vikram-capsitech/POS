6820b1e14ac67394f142d6c886a0a9062a146b7e
Vikram Rajpurohit <rajpurohitvikramdev@gmail.com>
2026-02-14 23:19:44 +0530

    chore: Remove all frontend and backend application code, UI components, styles, and configuration files.

--- backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getOrders, createOrder, updateOrder, addOrderItems, getOrderById } = require('../controllers/orderController');
-
-router.route('/').get(getOrders).post(createOrder);
-router.route('/:id').get(getOrderById).put(updateOrder);
-router.route('/:id/items').post(addOrderItems);
-
-module.exports = router;
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authUser, registerUser, getUsers } = require('../controllers/userController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.post('/login', authUser);
-router.route('/').post(registerUser).get(protect, admin, getUsers);
-
-module.exports = router;
