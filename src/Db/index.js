2c906f06551d6f5a4b7c0ca0ded9a3d0011b43f6
Vikram Rajpurohit <rajpurohitvikramdev@gmail.com>
2026-03-03 02:08:59 +0530

    feat: Add a fallback MongoDB connection URI for `mongoose.connect`.

--- src/Db/index.js
+++ src/Db/index.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    const connectionInstance = await mongoose.connect(
+      process.env.MONGODB_URI ??
+        "mongodb+srv://vikram:Vikram1436@cluster0.eaasq.mongodb.net/POS?retryWrites=true&w=majority",
+    );
     console.log(
       `✅ MongoDB Connected! Host: ${connectionInstance.connection.host}`,
     );
